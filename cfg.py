e4a192e223beda6f44837ad51dd3c187b4290a4e
Francesco Marconi <francesco.marconi@polimi.it>
2017-10-07 00:01:53 +0200

    Provide integration  as a module and support python3

--- cfg.py
+++ cfg.py
@@ -2,8 +2,6 @@ import re
 
 # 17/05/09 08:17:37.669 INFO SparkContext: Running Spark version 2.0.3-SNAPSHOT
 
-
-
 # 17/05/23 08:35:25.379 INFO TaskSetManager: Starting task 0.0 in stage 0.0 (TID 0, 10.0.0.12, partition 0, ANY, 5860 bytes)
 ## START STAGE
 # 17/05/23 08:36:00.792 INFO TaskSchedulerImpl: Adding task set 7.0 with 64 tasks
@@ -12,17 +10,18 @@ import re
 # 17/05/23 08:36:03.752 INFO TaskSchedulerImpl: Removed TaskSet 13.0, whose tasks have all completed, from pool
 
 
-SPARK_LOG_PATTERN = '^(\d{2}/\d{2}/\d{2}) (\d{2}\:\d{2}:\d{2}.\d+) (\S+) (\S+): (.+)'
+SPARK_LOG_PATTERN = '^(\d{2}/\d{2}/\d{2}) (\d{2}\:\d{2}:\d{2}(?:\.\d+)?) (\S+) (\S+): (.+)'
 REDUCED_PATTERN = '(\S+) (\S+): (.+)'
 
 START_JOB_MSG = "INFO TaskSetManager: Starting task 0.0 in stage 0.0"
 END_JOB_MSG = "INFO SparkUI: Stopped Spark web"
 
 ADD_TASKSET_MSG = "INFO TaskSchedulerImpl: Adding task set (\d+).\d* with (\d+) tasks"
-START_STAGE_MSG = "INFO TaskSetManager: Starting task 0.0 in stage (\d+).\d*"
+START_STAGE_MSG = "INFO TaskSetManager: Starting task 0.0 in stage (\d+).0"
 END_STAGE_MSG = "INFO TaskSchedulerImpl: Removed TaskSet (\d+)"
 
-
+START_TASK_MSG = "INFO TaskSetManager: Starting task (\d+).0 in stage (\d+).0 \(TID (\d+)"
+END_TASK_MSG = "INFO TaskSetManager: Finished task (\d+).0 in stage (\d+).0 \(TID (\d+)\) in (\d+) ms"
 
 start_match = re.search(REDUCED_PATTERN, START_JOB_MSG)
 START_LOG_LEVEL = start_match.group(1)
--- run.py
+++ run.py
@@ -4,13 +4,12 @@ import datetime
 import sys
 import os
 import json
-from collections import OrderedDict
+from functools import reduce
 
-import cfg
-from cfg import TIME_BOUND
-
-app_dir = sys.argv[1].strip(os.sep)
-app_name = sys.argv[2] if len(sys.argv) > 2 else app_dir.split(os.sep)[-1]
+if __name__ == "__main__":
+    import cfg
+else:
+    from . import cfg
 
 
 def json_serial(obj):
@@ -29,13 +28,14 @@ def parse_date_time(d, t):
     Returns:
         datetime: datetime object (ignore timezone)
     """
+    milliseconds = int(t[9:12]) * 1000 if t[9:12] else 0
     return datetime.datetime(int(d[0:2]),
                              int(d[3:5]),
                              int(d[6:8]),
                              int(t[0:2]),
                              int(t[3:5]),
                              int(t[6:8]),
-                             int(t[9:12])*1000)
+                             milliseconds)
 
 
 def parse_spark_logline(logline):
@@ -73,110 +73,168 @@ def populate_stage_time_struct(msg, time_struct, job_info, first_ev):
     match_end_stage = (re.search(cfg.get_log_fields(cfg.END_STAGE_MSG)["descr"],
                                  msg["description"]),
                        "remove_taskset")
+    match_end_task = re.search(cfg.get_log_fields(cfg.END_TASK_MSG)["descr"], msg["description"])
 
-    match = filter(lambda (x, y): x is not None, [match_add_taskset,
+    match = list(filter(lambda x: x[0] is not None, [match_add_taskset,
                                                   match_start_stage,
-                                                  match_end_stage])
+                                                  match_end_stage]))
     if len(match) > 0:
         stage_id, label = match[0][0].group(1), match[0][1]
-        print "{}\t{}-{}".format((msg["timestamp"] - first_ev).total_seconds() * 1000, stage_id, label)
+        print("{}\t\t{}-{}".format((msg["timestamp"] - first_ev).total_seconds() * 1000, stage_id, label))
         if stage_id not in time_struct:
             time_struct[stage_id] = {}
         time_struct[stage_id][label] = msg["timestamp"]
-    elif re.search(cfg.get_log_fields(cfg.END_JOB_MSG)["descr"], msg["description"]) is not None:
-        job_info["end_job"] = msg["timestamp"]
+#    elif re.search(cfg.get_log_fields(cfg.END_JOB_MSG)["descr"], msg["description"]) is not None:
+#        job_info["end_job"] = msg["timestamp"]
+    elif match_end_task is not None:
+        if "sum_of_task_durations_ta_master" not in time_struct[match_end_task.group(2)]:
+            time_struct[match_end_task.group(2)]["sum_of_task_durations_ta_master"] = 0
+        time_struct[match_end_task.group(2)]["sum_of_task_durations_ta_master"] += int(match_end_task.group(4))
+        job_info["latest_event"] = msg["timestamp"]
 
 
-stage_time_struct = {}
-job_time_struct = {}
+def main(app_dir, app_name=None):
+    stage_time_struct = {}
+    job_time_struct = {}
 
-# open spark log file
-with open(app_dir + os.sep + 'app.dat') as log_file:
-    spark_log_lines = map(lambda x: parse_spark_logline(x), log_file.readlines())
+    if app_name is None:
+        app_name = app_dir.split(os.sep)[-1]
 
-# open gazzella log file
-with open(app_dir + os.sep + 'app.json') as stages_file:
-    stages = json.load(stages_file)
+    # open spark log file
+    print("opening {}...".format(app_dir + os.sep + 'app.dat'))
+    with open(app_dir + os.sep + 'app.dat') as log_file:
+        spark_log_lines = map(lambda x: parse_spark_logline(x), log_file.readlines())
 
-# open config.json
-with open(app_dir + os.sep + 'config.json') as spark_config_file:
-    spark_config = json.load(spark_config_file)
-    num_cores = spark_config["Control"]["CoreVM"] * spark_config["Control"]["MaxExecutor"]
+    # open ta_executor log file
+    print("opening {}...".format(app_dir + os.sep + 'app.json'))
+    with open(app_dir + os.sep + 'app.json') as stages_file:
+        stages = json.load(stages_file)
 
-# get first event
-first_event = next(i for i in spark_log_lines if i["timestamp"] is not None)["timestamp"]
-print "FIRST EVENT", first_event
+    # open config.json
+    print("opening {}...".format(app_dir + os.sep + 'config.json'))
+    with open(app_dir + os.sep + 'config.json') as spark_config_file:
+        spark_config = json.load(spark_config_file)
+        num_cores = spark_config["Control"]["CoreVM"] * spark_config["Control"]["MaxExecutor"]
 
-# extract stage-specific times
-for i in spark_log_lines:
-    populate_stage_time_struct(i, stage_time_struct, job_time_struct, first_event)
+    # get first event
+    first_event = next(i for i in spark_log_lines if i["timestamp"] is not None)["timestamp"]
+    print("FIRST EVENT", first_event)
+    job_time_struct['latest_event'] = first_event
 
-for s, t in stage_time_struct.items():
-    if s != "job_time_struct":
-        t["totalduration"] = (t["remove_taskset"] - t["add_taskset"]).total_seconds() * 1000
-        t["tasks_only"] = (t["remove_taskset"] - t["start_taskset"]).total_seconds() * 1000
-        t["overhead"] = (t["start_taskset"] - t["add_taskset"]).total_seconds() * 1000
-        t["gazzella"] = stages[s]["duration"]/num_cores  # only valid when num_cores==num_tasks
-        print """ STAGE {}:
-                  totalduration:\t{}
-                  tasks_only:\t\t{}
-                  overhead:\t\t{}\t({:.2f} %)
-                  GAZZELLA:\t\t{}""".format(s,
-                                       t["totalduration"],
-                                       t["tasks_only"],
-                                       t["overhead"],
-                                       t["overhead"]/t["totalduration"]*100,
-                                       t["gazzella"])
-total_overhead = reduce(lambda x, y: x + y, [z["overhead"] for z in stage_time_struct.values()])
-total_tasks_only = reduce(lambda x, y: x + y, [z["tasks_only"] for z in stage_time_struct.values()])
-total_totalduration = reduce(lambda x, y: x + y, [z["totalduration"] for z in stage_time_struct.values()])
-total_gazzella_stages = reduce(lambda x, y: x + y, [z["gazzella"] for z in stage_time_struct.values()])
+    # extract stage-specific times
+    for i in spark_log_lines:
+        populate_stage_time_struct(i, stage_time_struct, job_time_struct, first_event)
 
-job_time_struct["start_job"] = stage_time_struct['0']["start_taskset"]
-job_duration = (job_time_struct["end_job"] - job_time_struct["start_job"]).total_seconds() * 1000
-job_time_struct["total_overhead"] = total_overhead
-job_time_struct["total_tasks_only"] = total_tasks_only
-job_time_struct["total_totalduration"] = total_totalduration
-job_time_struct["total_gazzella_stages"] = total_gazzella_stages
+    for s, t in stage_time_struct.items():
+        if s != "job_time_struct":
+            t["add_to_end_taskset"] = (t["remove_taskset"] - t["add_taskset"]).total_seconds() * 1000
+            t["start_to_end_taskset"] = (t["remove_taskset"] - t["start_taskset"]).total_seconds() * 1000
+            t["add_to_start_taskset_overhead"] = (t["start_taskset"] - t["add_taskset"]).total_seconds() * 1000
+            t["sum_of_task_durations_ta_executor"] = stages[s]["duration"]
+            t["avg_task_duration_ta_executor"] = stages[s]["duration"]/stages[s]["numtask"]
+            t["avg_stage_duration_ta_executor"] = stages[s]["duration"] / num_cores + 1
+            t["avg_task_duration_ta_master"] = t["sum_of_task_durations_ta_master"] / stages[s]["numtask"]
+            t["avg_stage_duration_ta_master"] = t["sum_of_task_durations_ta_master"] / num_cores + 1
+            diff_sum_of_task_durations = float(t["sum_of_task_durations_ta_master"] - t["sum_of_task_durations_ta_executor"])
+            diff_avg_task_duration = float(t["avg_task_duration_ta_master"] - t["avg_task_duration_ta_executor"])
+            diff_avg_stage_duration = float(t["avg_stage_duration_ta_master"] - t["avg_stage_duration_ta_executor"])
+            print(""" STAGE {} \t({}):
+                  SUM_OF_TASK_DURATIONS:
+                   - ta_executor:\t{}
+                   - ta_master:\t{}
+                   - diff:\t\t{}\t(+{:.2f} %)
+                  AVG_TASK_DURATION:
+                   - ta_executor:\t{}
+                   - ta_master:\t{}
+                   - diff:\t\t{}
+                  AVG_STAGE_DURATION:
+                   - ta_executor:\t{}
+                   - ta_master:\t{}
+                   - diff:\t\t{}
+                  add_to_end_taskset:\t\t{}
+                  start_to_end_taskset:\t\t{}
+                  add_to_start_taskset_overhead:\t{}\t({:.2f} %)
+                  """\
+                .format(s,
+                        stages[s]["name"],
+                        t["sum_of_task_durations_ta_executor"],
+                        t["sum_of_task_durations_ta_master"],
+                        diff_sum_of_task_durations,
+                        diff_sum_of_task_durations/t["sum_of_task_durations_ta_executor"]*100,
+                        t["avg_task_duration_ta_executor"],
+                        t["avg_task_duration_ta_master"],
+                        diff_avg_task_duration,
+                        t["avg_stage_duration_ta_executor"],
+                        t["avg_stage_duration_ta_master"],
+                        diff_avg_stage_duration,
+                        t["add_to_end_taskset"],
+                        t["start_to_end_taskset"],
+                        t["add_to_start_taskset_overhead"],
+                        t["add_to_start_taskset_overhead"]/t["add_to_end_taskset"]*100))
 
-print "TOTAL TASKS_ONLY:\t{}".format(total_tasks_only)
-print "TOTAL OVERHEAD:\t\t{}\t({:.2f} %)".format(total_overhead, total_overhead/total_totalduration*100)
-print "TOT WITH OVERHEAD:\t{}".format(total_totalduration)
-print "JOB DURATION:\t\t{}".format(job_duration)
-print "TOTAL GAZZELLA:\t\t{} - {}".format(stages['0']['totalduration']/num_cores, total_gazzella_stages)
+    add_to_start_taskset_overhead = reduce(lambda x, y: x + y, [z["add_to_start_taskset_overhead"] for z in stage_time_struct.values()])
+    total_start_to_end_taskset = reduce(lambda x, y: x + y, [z["start_to_end_taskset"] for z in stage_time_struct.values()])
+    total_add_to_end_taskset = reduce(lambda x, y: x + y, [z["add_to_end_taskset"] for z in stage_time_struct.values()])
+    total_ta_executor_stages = reduce(lambda x, y: x + y, [z["avg_stage_duration_ta_executor"] for z in stage_time_struct.values()])
+    total_tasks_only_ta_master = reduce(lambda x, y: x + y, [z["avg_stage_duration_ta_master"] for z in stage_time_struct.values()])
+
+    job_time_struct["start_job"] = stage_time_struct['0']["start_taskset"]
+    job_duration = (job_time_struct["latest_event"] - job_time_struct["start_job"]).total_seconds() * 1000
+    job_time_struct["total_add_to_start_taskset_overhead"] = add_to_start_taskset_overhead
+    job_time_struct["total_start_to_end_taskset"] = total_start_to_end_taskset
+    job_time_struct["total_add_to_end_taskset"] = total_add_to_end_taskset
+    job_time_struct["total_ta_executor_stages"] = total_ta_executor_stages
+    job_time_struct["total_tasks_only_ta_master"] = total_tasks_only_ta_master
+    job_time_struct["actual_job_duration"] = job_duration
+
+    print("TOTAL TASKS_ONLY:\t{}".format(total_start_to_end_taskset))
+    print("TOTAL OVERHEAD:\t\t{}\t({:.2f} %)".format(add_to_start_taskset_overhead, add_to_start_taskset_overhead / total_add_to_end_taskset * 100))
+    print("TOT WITH OVERHEAD:\t{}".format(total_add_to_end_taskset))
+    print("JOB DURATION:\t\t{}".format(job_duration))
+    print("TOTAL TA_EXECUTOR:\t\t{} - {}".format(stages['0']['totalduration']/num_cores, total_ta_executor_stages))
 
 
-SPARK_CONTEXT = {
-    "app_name" : "{}_c{}_t{}_{}l_d{}_tc_{}_n_rounds_{}".format(app_name,
-                                                               num_cores,
-                                                               cfg.TIME_BOUND,
-                                        "no_" if cfg.NO_LOOPS else "",
-                                                               job_duration,
-                                        "parametric" if cfg.PARAMETRIC_TC else "by20",
-                                        "by2"),
-#        "app_dir_acceleration_0_1000_c48_t40_no-l_d133000_tc_parametric_forall_nrounds_TEST",
-    "verification_params" :
-    {
-        "plugin": cfg.PLUGIN,
-        "time_bound" : cfg.TIME_BOUND,
-    	"parametric_tc": cfg.PARAMETRIC_TC,
-        "no_loops" : cfg.NO_LOOPS
-    },
-    "tot_cores" : num_cores,
-    "analysis_type" : "feasibility",
-    "deadline" : job_duration,
-    "max_time" : job_duration,
-    "tolerance": cfg.TOLERANCE,
-    "stages": stages
-}
+    SPARK_CONTEXT = {
+        "app_name" : "{}_c{}_t{}_{}l_d{}_tc_{}_n_rounds_{}".format(app_name,
+                                                                   num_cores,
+                                                                   cfg.TIME_BOUND,
+                                            "no_" if cfg.NO_LOOPS else "",
+                                                                   job_duration,
+                                            "parametric" if cfg.PARAMETRIC_TC else "by20",
+                                            "by2"),
+    #        "app_dir_acceleration_0_1000_c48_t40_no-l_d133000_tc_parametric_forall_nrounds_TEST",
+        "verification_params" :
+        {
+            "plugin": cfg.PLUGIN,
+            "time_bound" : cfg.TIME_BOUND,
+            "parametric_tc": cfg.PARAMETRIC_TC,
+            "no_loops" : cfg.NO_LOOPS
+        },
+        "tot_cores" : num_cores,
+        "analysis_type" : "feasibility",
+        "deadline" : job_duration,
+        "max_time" : job_duration,
+        "tolerance": cfg.TOLERANCE,
+        "stages": stages
+    }
 
-out_path_context = app_dir+os.sep+app_name+'_context.json'
-print "dumping to {}".format(out_path_context)
-with open(out_path_context, 'w') as outfile:
-    json.dump(SPARK_CONTEXT, outfile, indent=4, sort_keys=True)
+    out_path_context = app_dir+os.sep+app_name+'_context.json'
+    print("dumping to {}".format(out_path_context))
+    with open(out_path_context, 'w') as outfile:
+        json.dump(SPARK_CONTEXT, outfile, indent=4, sort_keys=True)
 
-out_path_time_structs = app_dir+os.sep+app_name+'_time_analysis.json'
-print "dumping to {}".format(out_path_time_structs)
-with open(out_path_time_structs, 'w') as outfile:
-    json.dump({"stages": stage_time_struct, "job": job_time_struct},
-              outfile, indent=4, sort_keys=True, default=json_serial)
+    out_path_time_structs = app_dir+os.sep+app_name+'_time_analysis.json'
+    print("dumping to {}".format(out_path_time_structs))
+    with open(out_path_time_structs, 'w') as outfile:
+        json.dump({"stages": stage_time_struct, "job": job_time_struct},
+                  outfile, indent=4, sort_keys=True, default=json_serial)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        input_app_dir = sys.argv[1].strip(os.sep)
+        input_app_name = sys.argv[2] if len(sys.argv) > 2 else input_app_dir.split(os.sep)[-1]
+    else:
+        print('ERROR: You must provide at least one argument')
+        sys.exit(0)
+    main(app_dir=input_app_dir, app_name=input_app_name)
